667f8574e5a4170494a2563f9558ef86666a6e45
Chengjing <cyuan@ucsd.edu>
2022-08-31 22:43:07 -0700

    add message filter

--- src/livox_pcd_projection/include/livox_pcd_projection/projectCloud.hpp
+++ src/livox_pcd_projection/include/livox_pcd_projection/projectCloud.hpp
@@ -24,6 +24,10 @@
 #include <geometry_msgs/msg/pose2_d.hpp>
 #include <dist_msg/msg/dist.hpp>
 
+#include <message_filters/subscriber.h>
+#include <message_filters/synchronizer.h>
+#include <message_filters/sync_policies/approximate_time.h>
+
 struct projected_pt{
     int u; // image frame x
     int v; // image frame y
@@ -40,11 +44,15 @@ public:
     void cloudCallback(const livox_interfaces::msg::CustomMsg& msg);
     // void detectionsCallback(const vision_msgs::msg::Detection2DArray::SharedPtr& msg);
     void detectionsCallback(const vision_msgs::msg::Detection2DArray& msg);
+    void callback(const sensor_msgs::msg::Image::ConstSharedPtr &image_msg, const livox_interfaces::msg::CustomMsg::ConstSharedPtr &cloud_msg, const vision_msgs::msg::Detection2DArray::ConstSharedPtr &detection_msg) {
+
 
     string intrinsic_path, extrinsic_path;
     rclcpp::Publisher<dist_msg::msg::Dist>::SharedPtr chatter_pub;
-    rclcpp::Subscription<livox_interfaces::msg::CustomMsg>::SharedPtr cloud_sub;
-    rclcpp::Subscription<vision_msgs::msg::Detection2DArray>::SharedPtr detection_sub;
+    message_filters::Subscriber<livox_interfaces::msg::CustomMsg>::SharedPtr cloud_sub;
+    message_filters::Subscriber<vision_msgs::msg::Detection2DArray>::SharedPtr detection_sub;
+    message_filters::Subscriber<sensor_msgs::msg::Image>::SharedPtr image_sub;
+
 
 };
 
